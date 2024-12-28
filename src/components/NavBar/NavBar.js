130dc4f345fc3d9b5a55970a7773e19fe450f0c7
farhansherinvm <farhanasherinvm@gmail.com>
2024-12-28 17:27:20 +0530

    axios calling set

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,21 +10,33 @@ function NavBar() {
        axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
         console.log(response.data.results[0])
         setMovie(response.data.results[0])
-       })
-  }
-)
+      });
+    }, []); // Dependency array to avoid infinite loop
+  
+    const backgroundImage = movie
+    ? `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`
+    : `url('../../assets/images/IN-en-20241216-TRIFECTA-perspective_915a9055-68ad-4e81-b19a-442f1cd134dc_large.jpg')`;
+
   return (
-    <div className='navbar'>
+    <div className='navbar'
+    style={{
+      backgroundImage: backgroundImage,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '70vh',
+      width: '100%',
+      color: 'white',
+    }}>
        <img className='logo' src={logo} alt="Site logo" />
        <img className='avathar' src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png' alt='avathar'/>
        <div className='content'>
-       <h1 className='tittle'>{movie?.title}</h1>
+       <h1 className='tittle'>{movie?.name || 'Loading...'}</h1>
 
           <div className='buttons'>
             <button className='button'>play</button>
             <button className='button'>my list</button>
           </div>
-          <h1 className='discription'>A slow zoom-in shot focuses on a handwritten letter lying on a table, smudged with teardrops. The camera lingers on the ink bleeding into the paper, symbolizing the emotional weight of the moment.</h1>
+          <h1 className='discription'>{movie?.overview}</h1>
        </div>
        <div className="fade"></div>
     </div>
