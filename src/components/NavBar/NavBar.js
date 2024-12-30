15833d1f7ed38473942b00394d5309c7190bd00f
farhansherinvm <farhanasherinvm@gmail.com>
2024-12-31 03:36:17 +0530

    navimg callwith const

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,41 +2,33 @@ import './NavBar.css';
 import React, {useEffect, useState} from 'react'
 import logo from '../../assets/images/Netflix_Logo_PMS.png';
 import axios from '../../axios';
-import { API_KEY } from '../../constants/constants';
+import { API_KEY , baseimageUrl} from '../../constants/constants';
 function NavBar() {
   const [movie, setMovie]=useState()
 
   useEffect(()=>{
        axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-        console.log(response.data.results[0])
-        setMovie(response.data.results[0])
+        console.log(response.data.results[5])
+        setMovie(response.data.results[5])
       });
-    }, []); // Dependency array to avoid infinite loop
+    }, []);
   
-    const backgroundImage = movie
-    ? `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`
-    : `url('../../assets/images/IN-en-20241216-TRIFECTA-perspective_915a9055-68ad-4e81-b19a-442f1cd134dc_large.jpg')`;
-
+    
   return (
     <div className='navbar'
     style={{
-      backgroundImage: backgroundImage,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '70vh',
-      width: '100%',
-      color: 'white',
+      backgroundImage:`url(${baseimageUrl+movie?.backdrop_path || ""})`
     }}>
        <img className='logo' src={logo} alt="Site logo" />
        <img className='avathar' src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png' alt='avathar'/>
        <div className='content'>
-       <h1 className='tittle'>{movie?.name || 'Loading...'}</h1>
+       <h1 className='tittle'>{movie?.name || movie?.title ||'Loading...'}</h1>
 
           <div className='buttons'>
             <button className='button'>play</button>
             <button className='button'>my list</button>
           </div>
-          <h1 className='discription'>{movie?.overview}</h1>
+          <h1 className='discription'>{movie?.overview || ""}</h1>
        </div>
        <div className="fade"></div>
     </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,3 @@
 export const baseUrl="https://api.themoviedb.org/3";
 export const API_KEY="ed3f1c607297ddbde5c4a35fec3958ab";
+export const baseimageUrl="https://image.tmdb.org/t/p/original"
